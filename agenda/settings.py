96a187eebd7558ece055a79abff2323b99c08dc3
marcosqwo <cavalinmarcos@hotmail.com>
2025-10-09 15:09:49 -0300

    pronto

--- agenda/settings.py
+++ agenda/settings.py
@@ -152,6 +152,7 @@ STATIC_URL = 'static/'
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [BASE_DIR / 'static']
